90042a6f1fa011fc5345c1945f52d6bf5fa76f4a
c1529 <104741605+c1529@users.noreply.github.com>
2026-02-07 22:25:50 +0800

    Update day_1.c

--- c/day_1.c
+++ c/day_1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<stdlib.h>
+void bar(void) {
+	printf("bar begin\n");
+	exit(123);//直接退出
+	printf("bar end\n");
+}
 void swap(int *a, int *b) {//传递的是地址,直接修改内存中的数值
 	int temp = *a;
 	*a = *b;
@@ -11,6 +17,9 @@ void foo(void) {//局部静态变量会保留函数上一次的值
 	foo();
 }
 int main() {
-	foo();
+	/*foo();*/
+	printf("mian begin\n");
+	bar();
+	printf("main end\n");
 	return 0;
 }
