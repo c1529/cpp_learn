29f7216808327ab4c8fb7f706bb44861631defb9
c1529 <104741605+c1529@users.noreply.github.com>
2026-03-03 01:14:03 +0800

    Update 第一次作业.c

--- c/作业/第一次作业.c
+++ c/作业/第一次作业.c
@@ -4,15 +4,19 @@
 
 #define SIZE(a) (sizeof(a)/sizeof(a[0]))
 void find_two_largest(int arr[], int n, int* largest, int* second_largest);
+void split_time(long total_second, int* hour, int* minute, int* second);
 
 int main() {
-    int largest, second_largest;
-    int arr[] =  {
-        3,6,4,1,99,5,7,8,23
-    }
-    ;
-    find_two_largest(arr, SIZE(arr), &largest, &second_largest);
-    printf("largest = %d ,second_largest = %d\n", largest, second_largest);
+    //第一题
+    //int largest, second_largest;
+    //int arr[] =  {3, 6, 4, 1, 99, 5, 7, 8, 23};
+    //find_two_largest(arr, SIZE(arr), &largest, &second_largest);
+    //printf("largest = %d ,second_largest = %d\n", largest, second_largest);
+    //第二题
+    long total_second = 9527;
+    int hour, minute, second;
+    split_time(total_second, &hour, &minute, &second);
+    printf("hour = %d,minute = %d,second = %d\n", hour, minute, second);
     return 0;
 }
 
@@ -32,4 +36,10 @@ void find_two_largest(int arr[], int n, int* largest, int* second_largest) {
             *second_largest = arr[i];
         }
     }
+}
+
+void split_time(long total_second, int* hour, int* minute, int* second) {
+    *second = total_second % 60;
+    *minute = (total_second / 60) % 60;
+    *hour = (total_second / 3600) % 24;
 }
