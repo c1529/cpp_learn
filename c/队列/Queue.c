2c4c9b616a12a6d33e940d510b177d83cb72e57e
c1529 <104741605+c1529@users.noreply.github.com>
2026-03-09 16:24:59 +0800

    Implement basic queue operations in Queue.c

---
+++ c/队列/Queue.c
@@ -0,0 +1,52 @@
+#include "Queue.h"
+
+Queue* queue_create() {
+    //创建空队列
+    return calloc(1, sizeof(Queue));
+}
+
+void queue_destroy(Queue* q) {
+    free(q);
+}
+
+void queue_push(Queue* q, int val) {
+
+    if (queue_full(q)) {
+        printf("Your queue is full.\n");
+        exit(1);
+    }
+    q->elements[q->rear] = val;
+    q->rear = (q->rear + 1) % N;
+    q->size++;
+}
+
+int queue_pop(Queue* q) {
+
+    if (queue_empty(q)) {
+        printf("Your queue is empty.\n");
+        exit(1);
+    }
+    int res = q->elements[q->front];
+    q->front = (q->front + 1) % N;
+    q->size--;
+    return res;
+}
+
+int queue_peek(Queue* q) {
+
+    if (queue_empty(q)) {
+        printf("Your queue is empty.\n");
+        exit(1);
+    }
+    else {
+        return q->elements[q->front];
+    }
+}
+
+bool queue_empty(Queue* q) {
+    return q->size == 0;
+}
+
+bool queue_full(Queue* q) {
+    return q->size == N;
+}
